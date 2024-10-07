51dee34b87ecebfb3b6946652ad7f0bf4abda760
Phillip <philmography2010@gmail.com>
2024-10-07 18:54:20 -0500

    GA1 Complete Update

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -105,7 +105,6 @@ public class JobData {
                     if (!checkJobs.contains(job)) {
                         checkJobs.add(job);
                     }
-
                 }
             }
 
